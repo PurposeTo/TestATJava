5a759ff19d5ba066b184676a1b29ffd0cc3734be
PurposeTo <firecrystal1999@gmail.com>
2020-04-22 14:44:07 +0300

    Update GoogleSearch test

--- src/test/java/GoogleSearchingTest.java
+++ src/test/java/GoogleSearchingTest.java
@@ -2,37 +2,33 @@ import Pages.BankOpenPage;
 import Pages.GoogleResultsPage;
 import Pages.GoogleSearchPage;
 import Settings.SelenideDriverSettings;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
 
 public class GoogleSearchingTest extends SelenideDriverSettings {
 
     @Test
     public void RunGoogleSearchingTest() {
-        String ExceptedUrl = "www.open.ru";
+        String exceptedUrl = "www.open.ru";
 
         GoogleSearchPage searchPage = GoogleSearchPage.getNew();
         GoogleResultsPage resultsPage = searchPage.search("Открытие");
 
-        // Для того, что бы "запустить" метод find, необходимо выполнить после (!) любое действие
-        // Не кликает/ кликает не туда
-        resultsPage.results().find(text(ExceptedUrl)).click();
+        // Проверка на то, что среди элементов есть нужный. Вызывает Assert
+        SelenideElement exceptedElement = resultsPage.results().findBy(text(exceptedUrl)).shouldBe();
 
-    }
-
-
-    @Test
-    public void RunBankOpenTest() {
-        BankOpenPage bankOpenPage = BankOpenPage.getNew();
+        // Кликаем на заголовок
+        exceptedElement.find(By.cssSelector("#rso .r")).click();
+        BankOpenPage bankOpenPage = page(BankOpenPage.class);
 
         // Сравнить, что курс покупки больше чем курс продажи для USD
         Assert.assertTrue(bankOpenPage.getUSDRateSale() > bankOpenPage.getUSDRatePurchase());
-
         // Сравнить, что курс покупки больше чем курс продажи для EUR
         Assert.assertTrue(bankOpenPage.getEURRateSale() > bankOpenPage.getEURRatePurchase());
-
     }
 }
